1563af701e9720313f57e7e156ad5f381f0152ce
Lucrecia <lucreciapereyra439@gmail.com>
2025-03-23 19:01:40 -0300

    Se añadieron comentarios

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -25,11 +25,15 @@ def calcular_factoriales(desde, hasta):
     for num in range(desde, hasta + 1):
         print("Factorial", num, "! es", factorial(num))
 
+#Verifica si se paso un argumento en la linea de comandos
 if len(sys.argv) == 1:
+   #Si no hay argumentos, solicita la entrada manualmente
    rango = input("Ingrese un número o un rango (ej. 4-8, -10, 10-): ")
 else:
+    #Si hay argumento en la linea de comandos, lo usa
     rango= sys.argv[1]
 
+#Procesa la entrada para determinar si es un numero unico o un rango
 if "-" in rango:  # Si es un rango
     if rango.startswith("-"):  # Caso de "-hasta"
         hasta = int(rango[1:])
@@ -42,4 +46,5 @@ if "-" in rango:  # Si es un rango
 else:  # Si es un solo número
     desde = hasta = int(rango)
 
+#Llama a la funcion para calcular factoriales en el rango determinado
 calcular_factoriales(desde, hasta)
